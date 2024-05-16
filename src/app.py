ce7f856bebd0595be9cd8b0835728e695d503581
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-05-16 21:59:13 +0000

    Entrega

--- src/app.py
+++ src/app.py
@@ -27,16 +27,49 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+@app.route('/member',methods=['POST'])
+def new_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg":"Debes enviar informacion en el body"}),400
+    if "first_name" not in body:
+        return jsonify({"msg":"Debes definir un first_name"}),400
+    if "id" not in body:
+        new_member_data={
+            "id":jackson_family._generateId(),
+            "first_name":body["first_name"],
+            "last_name":jackson_family.last_name,
+            "age":body["age"],
+            "lucky_numbers":body["lucky_numbers"]
+        }
+    else:
+        new_member_data={
+            "id":body["id"],
+            "first_name":body["first_name"],
+            "last_name":jackson_family.last_name,
+            "age":body["age"],
+            "lucky_numbers":body["lucky_numbers"]
+        }
+    jackson_family.add_member(new_member_data)
+    print(body)
+    return jsonify({"msg":"New member added"}),200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_person(id):
+    person=jackson_family.get_member(id)
+    response_body = person
+    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>',methods=['DELETE'])
+def del_person(id):
+    jackson_family.delete_member(id)
+    return jsonify({'done':True}),200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
